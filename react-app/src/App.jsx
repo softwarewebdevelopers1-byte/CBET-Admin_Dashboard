a80ff26885dc00a623b75e6919a6b4c6ccd47c50
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-03-12 12:16:17 +0300

    Changing logged users page path

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           }
         />
         <Route
-          path="/admin/users"
+          path="/users"
           element={
             <Dashboard>
               <UsersPage />
